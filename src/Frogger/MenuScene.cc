b0a41afa32eb55802ec5edfe6c283864f8a2bc88
Mangonels - Dylan Calaf <dylancalaflatham@gmail.com>
2016-12-10 13:38:04 +0100

    2 estados para la menu scene

--- src/Frogger/MenuScene.cc
+++ src/Frogger/MenuScene.cc
@@ -8,6 +8,8 @@ using namespace Logger;
 #define CELL_WIDTH 80
 #define CELL_HEIGHT 80
 
+bool initialmenu = true; //A bool is enough for checking the group of buttons we want.
+
 MenuScene::MenuScene(void) {
 	m_background = { { 0, 0, W.GetWidth(), W.GetHeight() }, ObjectID::BG_01 };
 }
@@ -33,16 +35,27 @@ void MenuScene::Update(void) {
 		Println("mxn: ", IM.GetMouseCoords());
 		//select difficulty panels
 		//easy
-		if (mouseCoords.x > 455 && mouseCoords.x < 576 && mouseCoords.y > 355 && mouseCoords.y < 415) {
-		SM.SetCurScene<GameScene>();
+		if (initialmenu) { //In order to make two different pannels on the same menu scene, we are checking if the player is switched to difficulty selection or not.
+			if (mouseCoords.x > 455 && mouseCoords.x < 576 && mouseCoords.y > 355 && mouseCoords.y < 415) {
+				initialmenu = false;
+			}
+			if (mouseCoords.x > 452 && mouseCoords.x < 584 && mouseCoords.y > 660 && mouseCoords.y < 715) {
+				//Cerrar ventana/salir del juego
+			}
 		}
-		//medium
-		else if (mouseCoords.x > 416 && mouseCoords.x < 610 && mouseCoords.y > 510 && mouseCoords.y < 570) {
-		SM.SetCurScene<GameScene>();
-		}
-		//hard
-		else if (mouseCoords.x > 452 && mouseCoords.x < 584 && mouseCoords.y > 660 && mouseCoords.y < 715) {
-			SM.SetCurScene<GameScene>();
+		else //We are on the difficulty selection
+		{
+			if (mouseCoords.x > 455 && mouseCoords.x < 576 && mouseCoords.y > 355 && mouseCoords.y < 415) {
+				SM.SetCurScene<GameScene>();
+			}
+			//medium
+			else if (mouseCoords.x > 416 && mouseCoords.x < 610 && mouseCoords.y > 510 && mouseCoords.y < 570) {
+				SM.SetCurScene<GameScene>();
+			}
+			//hard
+			else if (mouseCoords.x > 452 && mouseCoords.x < 584 && mouseCoords.y > 660 && mouseCoords.y < 715) {
+				SM.SetCurScene<GameScene>();
+			}
 		}
 	}
 
@@ -55,20 +68,36 @@ void MenuScene::Update(void) {
 
 void MenuScene::Draw(void) {
 	m_background.Draw(); // Render background
-	GUI::DrawTextShaded<FontID::FACTORY>("FROGGER",
-	{ W.GetWidth() >> 1, int(W.GetHeight()*.1f), 1, 1 },
-	{ 190, 0, 160 }, { 50, 200, 230 }); // Render score that will be different when updated
-	GUI::DrawTextShaded<FontID::FACTORY>("with enti crush typography lol",
-	{ W.GetWidth() >> 1, int(W.GetHeight()*.2f), 1, 1 },
-	{ 190, 0, 160 }, { 50, 200, 230 });
-
-	GUI::DrawTextBlended<FontID::CANDY>("Easy",
-	{ W.GetWidth() >> 1, int(W.GetHeight()*.5f), 1, 1 },
-	{ 115, 0, 180 }); // Render score that will be different when updated
-	GUI::DrawTextBlended<FontID::CANDY>("Medium",
-	{ W.GetWidth() >> 1, int(W.GetHeight()*.7f), 1, 1 },
-	{ 115, 0, 180 }); // Render score that will be different when updated
-	GUI::DrawTextBlended<FontID::CANDY>("Hard",
-	{ W.GetWidth() >> 1, int(W.GetHeight()*.9f), 1, 1 },
-	{ 115, 0, 180 }); // Render score that will be different when updated
+	if (initialmenu) {
+		GUI::DrawTextShaded<FontID::FACTORY>("EXTREME FROGGER",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.1f), 1, 1 },
+		{ 190, 0, 160 }, { 50, 200, 230 }); // Render score that will be different when updated
+		GUI::DrawTextShaded<FontID::FACTORY>("by Dylan Calaf and Daniel peco",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.2f), 1, 1 },
+		{ 190, 0, 160 }, { 50, 200, 230 });
+		GUI::DrawTextBlended<FontID::CANDY>("START",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.5f), 1, 1 },
+		{ 115, 0, 180 });
+		GUI::DrawTextBlended<FontID::CANDY>("LEAVE",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.7f), 1, 1 },
+		{ 115, 0, 180 });
+	}
+	else 
+	{
+		GUI::DrawTextShaded<FontID::FACTORY>("EXTREME FROGGER",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.1f), 1, 1 },
+		{ 190, 0, 160 }, { 50, 200, 230 });
+		GUI::DrawTextShaded<FontID::FACTORY>("by Dylan Calaf and Daniel peco",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.2f), 1, 1 },
+		{ 190, 0, 160 }, { 50, 200, 230 });
+		GUI::DrawTextBlended<FontID::CANDY>("Easy",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.5f), 1, 1 },
+		{ 115, 0, 180 });
+		GUI::DrawTextBlended<FontID::CANDY>("Medium",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.7f), 1, 1 },
+		{ 115, 0, 180 });
+		GUI::DrawTextBlended<FontID::CANDY>("Hard",
+		{ W.GetWidth() >> 1, int(W.GetHeight()*.9f), 1, 1 },
+		{ 115, 0, 180 });
+	}
 }
