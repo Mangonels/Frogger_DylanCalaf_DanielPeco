c82c614464da3c89132a8d73db15fedfb2460f49
dcalaf <dcalaf@S5-15>
2016-12-12 16:28:19 +0100

    get rekt XML extreme parsing edition

--- src/Frogger/IOManager.hh
+++ src/Frogger/IOManager.hh
@@ -11,19 +11,20 @@
 using namespace Logger;
 
 namespace IOManager {
-	/*
-	void TestXML(std::string &&filename) {
-	rapidxml::file<> xmlFile(RESOURCE_FILE(filename));
-	rapidxml::xml_document<> doc;
-	doc.parse<0>(xmlFile.data());
-	rapidxml::xml_node<> *root_node = doc.first_node("document");
-	for (rapidxml::xml_node<> * node = root_node->first_node("node"); node; node = node->next_sibling()) {
-	Println("Node: ",
-	node->first_attribute("att1")->value(),
-	" - ",
-	node->first_attribute("att2")->value());
+
+	void TestXML(std::string &&filename) { //pasas un string.
+		rapidxml::file<> xmlFile(RESOURCE_FILE(filename)); //carreguem filename a un a variable file de XML
+		rapidxml::xml_document<> doc; //creem un espai de memoria per a un document xml
+		doc.parse<0>(xmlFile.data()); //parsejem doc i hi asignem el contingut (data()) de xmlFile
+		rapidxml::xml_node<> *root_node = doc.first_node("levels"); //root node contindrá el primer node, que será <levels>
+		for (rapidxml::xml_node<> * node = root_node->first_node("node"); node; node = node->next_sibling()) {
+			Println("Node: ",
+				node->first_attribute("att1")->value(),
+				" - ",
+				node->first_attribute("att2")->value());
+		}
 	}
-	*/
+
 
 	// Loader function that takes level info for a grid
 	std::vector<std::vector<ObjectID>> LoadLevel(std::string &&filename, int &rows, int &cols) {
